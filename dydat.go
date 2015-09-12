8d3e4dd5896bb02e78b5e0e17e3b6d2ad93bc020
pastebt <maye1968@gmail.com>
2015-09-12 14:12:42 -0700

    dydat using atomic.Value

--- dydat.go
+++ dydat.go
@@ -1,12 +1,31 @@
-package dydat
+package main
 
 import (
-    "sync"
-    "sync/atom"
+//    "sync"
+    "sync/atomic"
 )
 
 
-type dydat struct {
-    dat interface{}
-    
+var dat atomic.Value
+var dch chan byte = make(chan byte, 1)
+
+
+func get_data() {
+        m := make(map[string]string)
+        m["abcd"] = "1234"
+        dat.Store(m)
+}
+
+func update() {
+    for range dch {
+        get_data()
+    }
+}
+
+
+func main() {
+    get_data()
+    go update()
+    m := dat.Load().(map[string]string)
+    println(m["abcd"])
 }
