a101f5ac751539adc1f68f4df0969ec611fc47f5
pastebt <maye1968@gmail.com>
2017-07-02 23:15:27 -0700

    db test driver

--- db.go
+++ db.go
@@ -6,15 +6,77 @@ import (
     "unsafe"
     "reflect"
     "database/sql"
+    "database/sql/driver"
 
- _  "github.com/go-sql-driver/mysql"
+_   "github.com/go-sql-driver/mysql"
 )
+
+
+/*********************test Driver***************************/
+type tResult struct {
+    liid int64
+    rwaf int64
+}
+func (r *tResult)LastInsertId() (int64, error) { return r.liid, nil }
+func (r *tResult)RowsAffected() (int64, error) { return r.rwaf, nil }
+
+
+type tRows struct {
+}
+func (r *tRows)Columns() []string { return nil }
+func (r *tRows)Close() error { return nil }
+func (r *tRows)Next(dest []driver.Value) error { return nil }
+
+
+type tStmt struct {
+    num int
+}
+func (s *tStmt)Close() error { return nil }
+func (s *tStmt)NumInput() int { return s.num }
+func (s *tStmt)Exec(args []driver.Value) (*tResult, error) {
+    t := tResult{liid: 10, rwaf: 1}
+    return &t, nil
+}
+func (s *tStmt)Query(args []driver.Value) (*tRows, error) {
+    return &tRows{}, nil
+}
+
+
+type tTx struct {
+}
+func (t *tTx)Commit() error { return nil }
+func (t *tTx)Rollback() error { return nil }
+
+
+type tstConn struct {
+}
+func (c *tstConn)Prepare(query string) (*tStmt, error) {
+    t := tStmt{}
+    return &t, nil
+}
+func (c *tstConn)Close() error { return nil }
+func (c *tstConn)Begin() (*tTx, error) {
+    t := tTx{}
+    return &t, nil
+}
+
+
+type tstDrv struct {
+}
+func (t *tstDrv)Open(name string) (*tstConn, error) {
+    tc := tstConn{}
+    return &tc, nil
+}
+
+/*********************test Driver***************************/
+
 
 
 type Dat struct {
     uid  int64
     ukey string
 }
+
 
 
 func main() {
